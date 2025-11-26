94fc148d8110cc90b25747acecdfab7d42d3e4da
Shalha-Mucha18 <ms.mucha18@gmail.com>
2025-11-27 03:00:59 +0600

    updated backend

--- api/shorts.py
+++ api/shorts.py
@@ -1,6 +1,6 @@
 from mangum import Mangum
 from backend.app.main import app
 
-handler = Mangum(app, lifespan="off", api_gateway_base_path="/api/shorts")
+handler = Mangum(app, lifespan="off")
 
 __all__ = ["handler"]
