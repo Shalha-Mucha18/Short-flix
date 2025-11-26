a40773b87ee985b8c883d3394f0f770c90df51ff
Shalha-Mucha18 <ms.mucha18@gmail.com>
2025-11-27 02:44:16 +0600

    updated the backend

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from backend.app.api.v1.routes import shorts
 
-api_router = APIRouter(prefix="/api/shorts")
-api_router.include_router(shorts.router)
+api_router = APIRouter()
+api_router.include_router(shorts.router, prefix="")
 
 __all__ = ["api_router"]
